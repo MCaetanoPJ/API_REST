854410e09bd18665ab61a72cb69a2d041c7c01b9
Marcos Caetano <39209992+MCaetanoPJ@users.noreply.github.com>
2021-06-02 13:51:52 -0300

    Add files via upload

---
+++ API-REST-MVC/Controllers/PacienteController.cs
@@ -0,0 +1,216 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using API_REST_MVC.Model;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System.Linq;
+
+namespace API_REST_MVC.Controllers
+{
+    [Route("v1/Paciente")]
+    [Controller]
+    public class PacienteController : ControllerBase
+    {
+        private readonly Contexto _contexto;
+
+        public PacienteController(Contexto contexto)
+        {
+            _contexto = contexto;
+        }
+
+        // GET: v1/Paciente
+        [Route("")]
+        public ActionResult ListarPacientes()
+        {
+            try
+            {
+
+                var paciente = _contexto.Pacientes.ToList();
+                if (paciente.Count == 0)
+                {
+                    return NotFound("Nenhum Paciente está cadastrado na base de dados");
+                }
+                else
+                {
+                    return Ok(paciente);
+                }
+                //return Ok("Listando todos os pacientes");
+            }
+            catch (Exception erro)
+            {
+                return BadRequest(erro.StackTrace);
+            }
+        }
+
+        // GET: v1/Paciente/1
+        [HttpGet]
+        [Route("{Id}")]
+        public ActionResult ListarPacientePorId(int id)
+        {
+            try
+            {
+                var paciente = _contexto.Pacientes.FirstOrDefault(x => x.Id == id);
+                if (paciente == null)
+                {
+                    return NotFound("O Paciente com id "+id+" não foi encontrado no banco de dados");
+                }
+                return Ok(paciente);
+            }
+            catch (Exception erro)
+            {
+                return BadRequest(erro.StackTrace);
+            }
+        }
+
+        // POST: v1/Paciente/ListarPacientePorNomeCPF
+        [HttpPost]
+        [Route("ListarPacientePorNomeCPF")]
+        public ActionResult ListarPacientePorNomeCPF([FromBody] Paciente pacienteRecebido)
+        {
+            //try
+            //{
+                var paciente = _contexto.Pacientes.FirstOrDefault(x => x.Nome == pacienteRecebido.Nome || x.CPF == pacienteRecebido.CPF);
+                if (paciente == null)
+                {
+                    string aux = "O Paciente ";
+                    if (pacienteRecebido.CPF != null)
+                    {
+                        aux += " com CPF:"+pacienteRecebido.CPF;    
+                    }
+                    if (pacienteRecebido.Nome != null)
+                    {
+                        aux += " com Nome:" + pacienteRecebido.Nome;
+                    }
+                    aux += " não foi encontrado no banco de dados";
+
+                    return NotFound(aux);
+                }
+                else
+                {
+                    return Ok(paciente);
+                }
+                
+            /*}
+            catch (Exception erro)
+            {
+                return BadRequest(erro.StackTrace);
+            }*/
+        }
+
+        // POST: v1/Paciente/
+        [HttpPost]
+        [Route("")]
+        public ActionResult AdicionaPaciente([FromBody] Paciente paciente)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _contexto.Add(paciente);
+                    _contexto.SaveChanges();
+                    //return Ok(paciente);
+                    return Ok("O Paciente "+paciente.Nome+ " foi criado com sucesso!");
+                }
+                else
+                {
+                    return NotFound("Paciente não foi criado, os dados estão invalidos");
+                }
+            }
+            catch (Exception erro)
+            {
+                return BadRequest(erro.StackTrace);
+            }
+        }
+
+        // PUT: v1/Paciente/1
+        [HttpPut]
+        [Route("{id}")]
+        public ActionResult EditarPaciente(int id, [FromBody] Paciente pacienteNovo, Telefone telefoneNovo)
+        {
+            try
+            {
+                if (PacienteExiste(id))
+                {
+                    if (ModelState.IsValid)
+                    {
+                        var pacienteAntigo = _contexto.Pacientes.First(x => x.Id == id);
+
+                        //Atualizando os dados do paciente
+                        pacienteAntigo.Id = pacienteNovo.Id;
+                        pacienteAntigo.Nome = pacienteNovo.Nome;
+                        pacienteAntigo.CPF = pacienteNovo.CPF;
+                        pacienteAntigo.RG = pacienteNovo.RG;
+                        pacienteAntigo.CNS = pacienteNovo.CNS;
+                        pacienteAntigo.DataNascimento = pacienteNovo.DataNascimento;
+                        pacienteAntigo.Sexo = pacienteNovo.Sexo;
+                        pacienteAntigo.NomeMae = pacienteNovo.NomeMae;
+                        pacienteAntigo.Endereco = pacienteNovo.Endereco;
+                        pacienteAntigo.Bairro = pacienteNovo.Bairro;
+                        pacienteAntigo.CEP = pacienteNovo.CEP;
+                        pacienteAntigo.Estado = pacienteNovo.Estado;
+                        pacienteAntigo.Cidade = pacienteNovo.Cidade;
+
+                        //Atualizando os dados do telefone
+                        /*var telefoneAntigo = _contexto.Telefones.First(x => x.Id == id);
+                        telefoneAntigo.Id = telefoneNovo.Id;
+                        telefoneAntigo.DDD = telefoneNovo.DDD;
+                        telefoneAntigo.Numero = telefoneNovo.Numero;
+                        telefoneAntigo.Tipo = telefoneNovo.Tipo;*/
+                        
+                        _contexto.SaveChanges();
+                        return Ok(pacienteNovo);
+                    }
+                    else
+                    {
+                        return BadRequest("Dados inválidos");
+                    }
+                }
+                else{
+                    return NotFound("O paciente com id: "+id+" não foi encontrado no banco de dados");
+                }
+            }
+            catch (Exception erro)
+            {
+                return BadRequest(erro.StackTrace);
+            }
+        }
+
+        // DELETE: v1/Paciente/1
+        [HttpDelete]
+        [Route("{id}")]
+        public ActionResult ExcluirPacientePoId(int id)
+        {
+            try
+            {
+                if (PacienteExiste(id))
+                {
+                    var paciente = _contexto.Pacientes.FirstOrDefault(x => x.Id == id);
+                    _contexto.Remove(paciente);
+                    return Ok("Paciente com " + id + "Excluído com sucesso");
+                }
+                else
+                {
+                    return NotFound("Paciente com id: "+id+ " não encontrado");
+                }
+                
+            }
+            catch (Exception erro)
+            {
+                return BadRequest(erro.StackTrace);
+            }
+        }
+
+        private bool PacienteExiste(int id)
+        {
+            var paciente = _contexto.Pacientes.FirstOrDefault(x => x.Id == id);
+            if (paciente != null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+    }
+}
---
+++ API-REST-MVC/Migrations/20210522025546_Create_DataBase.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace API_REST_MVC.Migrations
+{
+    public partial class Create_DataBase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Pacientes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CPF = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    RG = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CNS = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DataNascimento = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Sexo = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    NomeMae = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Endereco = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Bairro = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CEP = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Estado = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Cidade = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pacientes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Telefones",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DDD = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Tipo = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Numero = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PacienteId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Telefones", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Telefones_Pacientes_PacienteId",
+                        column: x => x.PacienteId,
+                        principalTable: "Pacientes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PacientesTelefones",
+                columns: table => new
+                {
+                    pacienteId = table.Column<int>(type: "int", nullable: false),
+                    telefoneId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PacientesTelefones", x => new { x.pacienteId, x.telefoneId });
+                    table.ForeignKey(
+                        name: "FK_PacientesTelefones_Pacientes_pacienteId",
+                        column: x => x.pacienteId,
+                        principalTable: "Pacientes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PacientesTelefones_Telefones_telefoneId",
+                        column: x => x.telefoneId,
+                        principalTable: "Telefones",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PacientesTelefones_telefoneId",
+                table: "PacientesTelefones",
+                column: "telefoneId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Telefones_PacienteId",
+                table: "Telefones",
+                column: "PacienteId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PacientesTelefones");
+
+            migrationBuilder.DropTable(
+                name: "Telefones");
+
+            migrationBuilder.DropTable(
+                name: "Pacientes");
+        }
+    }
+}
